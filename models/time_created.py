101c7a28ca9e893757d46cafa596d11af05c9c12
Libby Lebyane <lebyane.lm@gmail.com>
2021-11-05 09:34:45 +0200

    Change the format of time_created on day d time

--- models/time_created.py
+++ models/time_created.py
@@ -1,14 +1,15 @@
 # dependencies
 import datetime
 
+
 class TimeCreated():
     def __init__(self, timestamp=None):
         date = datetime.datetime.now()
 
         if timestamp:
             date = datetime.datetime.fromtimestamp(timestamp)
-        
-        self.day = date.strftime('%d %m, %Y')
+
+        self.day = date.strftime('%d %A %B, %Y')
         self.time = date.strftime('%H:%M:%S')
 
         if timestamp:
