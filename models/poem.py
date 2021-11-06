6d35cbe8100834b5da9eb008d104c5e4a78df3c6
Libby Lebyane <lebyane.lm@gmail.com>
2021-11-06 14:08:29 +0200

    Modify to_dict method for Poem class since it has special attribute author with ObjectId type

--- models/poem.py
+++ models/poem.py
@@ -18,98 +18,105 @@ from helpers.poems import parse_poem_tags_string, get_tag_document, update_tag_d
 
 
 class Poem(Data):
-	def __init__(self, data, is_draft=False):
-		super().__init__()
+    def __init__(self, data, is_draft=False):
+        super().__init__()
 
-		self._id = bson.objectid.ObjectId()
-		self.thumbnail = data.get("thumbnail")
-		self.title = data.get("title")
-		self.body = data.get("body")
-		self.description = data.get("description")
-		self.author = data.get("author")
-		self.featured_poets = data.get("featured_poets")
-		self.audio_file = data.get("audio_file")
-		self.tags = parse_poem_tags_string(data.get("tags"))
-		self.collection = data.get("collection")
-		self.languages = data.get("language")
+        self._id = bson.objectid.ObjectId()
+        self.thumbnail = data.get("thumbnail")
+        self.title = data.get("title")
+        self.body = data.get("body")
+        self.description = data.get("description")
+        self.author = data.get("author")
+        self.featured_poets = data.get("featured_poets")
+        self.audio_file = data.get("audio_file")
+        self.tags = parse_poem_tags_string(data.get("tags"))
+        self.collection = data.get("collection")
+        self.languages = data.get("language")
 
+        if self.languages == None and is_draft == False:
+            # automatically detect the languages
+            self.languages = self.detect_language()
+            print(self.languages)
 
-		if self.languages == None and is_draft == False:
-			# automatically detect the languages
-			self.languages = self.detect_language()
-			print(self.languages)
+        # default values
+        self.read_time = self.get_read_time().__str__()
+        self.bookmarks_count = 0
+        self.likes = []
+        self.likes_count = 0
+        self.dislikes = []
+        self.dislikes_count = 0
+        self.shares_count = 0
+        self.views_count = 0
+        self.comments_count = 0
+        self.comments = []
+        self.reports = []
+        self.reports_count = 0
 
-		# default values
-		self.read_time = self.get_read_time().__str__()
-		self.bookmarks_count = 0
-		self.likes = []
-		self.likes_count = 0
-		self.dislikes = []
-		self.dislikes_count = 0
-		self.shares_count = 0
-		self.views_count = 0
-		self.comments_count = 0
-		self.comments = []
-		self.reports = []
-		self.reports_count = 0
+        other_possible_tags = []
+        if self.featured_poets:
+            other_possible_tags.append("features")
 
+        if self.audio_file:
+            other_possible_tags.append("audio")
 
-		other_possible_tags = []
-		if self.featured_poets:
-			other_possible_tags.append("features")
+        if self.collection:
+            other_possible_tags.append("collection")
 
-		if self.audio_file:
-			other_possible_tags.append("audio")
+        if self.languages and len(self.languages) > 0:
+            other_possible_tags.append(self.languages[0]["name"].lower())
 
-		if self.collection:
-			other_possible_tags.append("collection")
+        if self.tags == None:
+            # automatically come up with tags
+            self.tags = other_possible_tags
+        self.tags = [self.read_time, *self.tags, *other_possible_tags]
+        self.parse_current_tags()
 
-		if self.languages and len(self.languages) > 0:
-			other_possible_tags.append(self.languages[0]["name"].lower())
+        self._schema_version_ = 1.0
 
-		if self.tags == None:
-			# automatically come up with tags
-			self.tags = other_possible_tags
-		self.tags = [self.read_time, *self.tags, *other_possible_tags]
-		self.parse_current_tags()
+    def detect_language(self):
+        languages = Detector(self.body).languages
+        result_languages = list()
+        for language in languages:
+            if language.locale.getDisplayName() != language.locale.getName():
+                result_languages.append({
+                    "code": language.locale.getName(),
+                    "name": language.locale.getDisplayName()})
 
-		self._schema_version_ = 1.0
+        return result_languages
 
+    def get_read_time(self):
+        return readtime.of_text(self.body)
 
-	def detect_language(self):
-		languages = Detector(self.body).languages
-		result_languages = list()
-		for language in languages:
-			if language.locale.getDisplayName() != language.locale.getName():
-				result_languages.append({
-					"code" : language.locale.getName(),
-					"name" : language.locale.getDisplayName() })
+    def parse_current_tags(self):
+        if self.tags and len(self.tags) > 0:
+            for index, tag_name in enumerate(self.tags):
+                existing_tag = get_tag_document(tag_name)
 
-		return result_languages
+                if existing_tag == None:
+                    tag_data = Tag(tag_name)
+                    existing_tag = _Tag(
+                        **{**tag_data.__dict__, "_id": bson.objectid.ObjectId()})
 
+                if type(existing_tag) == _Tag:
+                    if self._id not in existing_tag.publishes:
+                        existing_tag.publishes.append(self._id)
+                        existing_tag.publishes_count = len(
+                            existing_tag.publishes)
+                        self.tags[index] = existing_tag._id
+                        existing_tag.save()
+                else:
+                    if self._id not in existing_tag["publishes"]:
+                        existing_tag["publishes"].append(self._id)
+                        existing_tag["publishes_count"] = len(
+                            existing_tag["publishes"])
+                        self.tags[index] = existing_tag["_id"]
+                        update_tag_document(existing_tag)
 
-	def get_read_time(self):
-		return readtime.of_text(self.body)
+    # A customized to dictionary function for the poem model
+    def to_dict(self) -> dict:
+        new_self_dictionary = Data.to_dict(self)
 
+        # Custom parameters to poem model
+        new_self_dictionary["author"] = str(new_self_dictionary["author"])
 
-	def parse_current_tags(self):
-		if self.tags and len(self.tags) > 0:
-			for index, tag_name in enumerate(self.tags):
-				existing_tag = get_tag_document(tag_name)
-
-				if existing_tag == None:
-					tag_data = Tag(tag_name)
-					existing_tag = _Tag(**{**tag_data.__dict__, "_id": bson.objectid.ObjectId()})
-
-				if type(existing_tag) == _Tag:
-					if self._id not in existing_tag.publishes:
-						existing_tag.publishes.append(self._id)
-						existing_tag.publishes_count = len(existing_tag.publishes)
-						self.tags[index] = existing_tag._id
-						existing_tag.save()
-				else:
-					if self._id not in existing_tag["publishes"]:
-						existing_tag["publishes"].append(self._id)
-						existing_tag["publishes_count"] = len(existing_tag["publishes"])
-						self.tags[index] = existing_tag["_id"]
-						update_tag_document(existing_tag)
+        return new_self_dictionary
